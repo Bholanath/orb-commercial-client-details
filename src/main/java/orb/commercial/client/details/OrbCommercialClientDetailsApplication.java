59382f96be78410fd19b9218b6a2a09bb4715cf0
Bholanath <bholanath.das21@gmail.com>
2021-07-08 19:24:29 +0530

    Add files via upload

---
+++ src/main/java/orb/commercial/client/details/OrbCommercialClientDetailsApplication.java
@@ -0,0 +1,13 @@
+package orb.commercial.client.details;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbCommercialClientDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbCommercialClientDetailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/orb/commercial/client/details/OrbCommercialClientDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package orb.commercial.client.details;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbCommercialClientDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
